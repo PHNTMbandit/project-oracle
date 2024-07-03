ce1a8f160fc32be4d9f59de32dd0e0b9153eddf4
Domenic Pittari <dom.pittari@gmail.com>
2024-07-03 13:15:39 +0900

    Installed nextjs

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { ThemeToggle } from "@/components/theme-toggle";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <ThemeToggle />
+      <Button>Click Me</Button>
+    </main>
+  );
+}
